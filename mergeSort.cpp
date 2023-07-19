cd1fab4f07b5f977f1d77301386d2caccafd34d1
kko <kkl24@mail.ru>
2023-07-19 22:21:59 +0300

    5-async

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -55,74 +55,62 @@ void merge(int* arr, int l, int m, int r)
    }
 }
 
-void mergeSort(promise<int>& p,int* arr, int l, int r){
+void mergeSort(int* arr, int l, int r){
    if(l>=r){
-      p.set_value(false);
       return;
    }
    int m=(l+r-1)/2;
 
-   promise<int> p1,p2;
-
    if(NUM_THREADS<MAX_THREADS){
 
-      NUM_THREADS+=2;
+      bool b1=true,b2=true;
 
-      thread t1(mergeSort, std::ref(p1), arr,l,m);
-      thread t2(mergeSort, std::ref(p2), arr,m+1,r);
+      future<void> f1,f2;
 
-      future<int> f1 = p1.get_future(),
-                  f2 = p2.get_future(); 
+      try{
+	      f1 = async(launch::async,mergeSort,  arr,l,m);
+          NUM_THREADS++;
+	  }catch(...){
+          mergeSort( arr,l,m);
+		  b1=false;
+	  }
 
-      //mergeSort(arr,l,m);
-      //mergeSort(arr,m+1,r);
+      try{
+          f2 = async(launch::async,mergeSort,  arr,m+1,r);
+          NUM_THREADS++;
+	  }catch(...){
+          mergeSort( arr,m+1,r);
+		  b2=false;	  
+      }
 
+
+      if(b1)
       try {
           f1.get();
+          NUM_THREADS--;
       } catch (...) {
           cout << "Some error in thread" << endl;
       }
 
+      if(b2)
       try {
           f2.get();
+          NUM_THREADS--;
       } catch (...) {
           cout << "Some error in thread" << endl;
       }
-
-      t1.join();
-      t2.join();
-
-      NUM_THREADS-=2;
 
    }else{
       cout <<  "::2" <<endl;
-      mergeSort(std::ref(p1), arr,l,m);
-      mergeSort(std::ref(p2), arr,m+1,r);
+      mergeSort( arr,l,m);
+      mergeSort( arr,m+1,r);
    }
 
    merge(arr,l,m,r);   
 
-   p.set_value(true);
 }
 
-void mergeSort1(int* arr, int l, int r){
-   promise<int> p1;
 
-   NUM_THREADS++;
-   thread t1(mergeSort, std::ref(p1), arr,l,r);
-
-   future<int> f1 = p1.get_future(); 
-
-   try {
-       f1.get();
-   } catch (...) {
-       cout << "Some error in thread" << endl;
-   }
-
-   t1.join();
-
-   NUM_THREADS--;
-}
 
 void out_arr(int* a,int sz){
    for(int i=0;i<sz;i++){
@@ -133,7 +121,7 @@ void out_arr(int* a,int sz){
 
 int main(){
    vector<int> v1={1,45,2,-100,6,13,474,-23,-89,-100,8,4000,5,-4,2,6,2,-8,8,0};
-   mergeSort1(v1.data(), 0, v1.size()-1);
+   mergeSort(v1.data(), 0, v1.size()-1);
    out_arr(v1.data(),v1.size());
    return 0;
 }
